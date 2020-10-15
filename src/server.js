7aa1076eed2436d12a83c4228e3a6b8a11d2d359
Kaic Pierre Silva <kpisilva@haoc.com.br>
2020-10-15 10:00:57 -0300

    Dia 4 NLW3 Pt.1
    Workshop do dia 4 - Primeira Hora

---
+++ src/server.js
@@ -0,0 +1,16 @@
+//importar dependencia
+const express = require('express')
+const path = require('path')
+
+//iniciando o express
+const server = express()
+server
+//utilizando os arquivos estáticos
+.use(express.static('public'))
+//criar rota
+.get('/', (request, response) => {
+    return response.sendFile(path.join(__dirname, 'views', 'index.html'))   
+})
+
+//ligar o servidor
+server.listen(5500)
